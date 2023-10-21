edc45abe8fccd64613e08fe72354f7f9c98881e3
Caique Daniel <caiquedaniel@outlook.com.br>
2023-10-21 00:59:36 -0300

    feat: add Yup for form validation

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,5 @@
 import {Field, Form, Formik, FormikProps} from "formik";
+import * as yup from "yup";
 
 type FormFieldsData = {
     name: string;
@@ -11,24 +12,31 @@ export function MyForm() {
         email: ''
     }
 
+    const validationSchema = yup.object({
+        name: yup.string().required(),
+        email: yup.string().required()
+    });
+
     const onSubmit = async (values: FormFieldsData) => {
         console.log(values);
         await new Promise((resolve) => setTimeout(() => resolve(true), 2000));
     }
 
     return (
-        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
             {
                 (formikProps: FormikProps<FormFieldsData>) => (
                     <Form>
                         <div>
                             <label>Nome:</label>
                             <Field name='name'/>
+                            <small style={{color: 'red'}}>{formikProps.errors.name}</small>
                         </div>
 
                         <div>
                             <label>Email:</label>
                             <Field name='email' type='email'/>
+                            <small style={{color: 'red'}}>{formikProps.errors.email}</small>
                         </div>
 
                         <div>
